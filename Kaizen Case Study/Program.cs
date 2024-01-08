46b91ea209226079f6edfc9c62a4eda1e796cc22
Alper Ozkan <alprozkan@gmail.com>
2024-01-08 22:17:47 +0300

    Initial Commit

---
+++ Kaizen Case Study/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    private static readonly char[] AllowedCharacters = { 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'K', 'L', 'M', 'N', 'P', 'R', 'T', 'X', 'Y', 'Z', '2', '3', '4', '5', '7', '9' };
+    private static readonly int CodeLength = 8;
+
+    static void Main(string[] args)
+    {
+        HashSet<string> codes = GenerateUniqueCodes(1000);
+
+        foreach (var code in codes)
+        {
+            Console.WriteLine($"Code: {code}, IsValid: {CheckCode(code)}");
+        }
+    }
+
+    private static HashSet<string> GenerateUniqueCodes(int numberOfCodes)
+    {
+        HashSet<string> codes = new HashSet<string>();
+        Random random = new Random();
+
+        while (codes.Count < numberOfCodes)
+        {
+            var code = GenerateCode(random);
+            codes.Add(code);
+        }
+
+        return codes;
+    }
+
+    private static string GenerateCode(Random random)
+    {
+        char[] codeChars = new char[CodeLength];
+
+        for (int i = 0; i < CodeLength; i++)
+        {
+            codeChars[i] = AllowedCharacters[random.Next(AllowedCharacters.Length)];
+        }
+
+        return new string(codeChars);
+    }
+
+    private static bool CheckCode(string code)
+    {
+        // Burada kodun yapısını kontrol eden algoritmayı tanımlayabiliriz.
+        // Örneğin, belirli bir desen, hash veya hesaplama yöntemi kullanılabilir.
+        // Bu örnekte basit bir yapısal kontrol yapılıyor.
+        if (code.Length != CodeLength) return false;
+
+        foreach (var ch in code)
+        {
+            if (Array.IndexOf(AllowedCharacters, ch) == -1)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Kazien Case Study 2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace OCRResultParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // JSON dosya yolu gerekli oldugunda degistirilmelidir
+            string jsonFilePath = @"C:\Users\alpro\OneDrive\Desktop\Kaizen\response.json";
+
+            try
+            {
+                string jsonContent = File.ReadAllText(jsonFilePath);
+
+                OCRResult ocrResult = JsonConvert.DeserializeObject<OCRResult>(jsonContent);
+
+                if (ocrResult != null)
+                {
+                    foreach (var item in ocrResult.Description)
+                    {
+                        Console.WriteLine($"Metin: {item.Text}");
+                        Console.WriteLine($"Konum: {string.Join(", ", item.BoundingBox)}");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("OCR sonucu boş veya hatalı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda hata mesajını yazdır
+                Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+    }
+
+    public class OCRResult
+    {
+        [JsonProperty("description")]
+        public List<DescriptionItem> Description { get; set; }
+    }
+
+    public class DescriptionItem
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("boundingBox")]
+        public List<int> BoundingBox { get; set; }
+    }
+}
